4793a6718dfccc41f8d72c91f327af91595df3fb
Sofiane <soufiane.belghit@gmail.com>
2018-12-12 14:43:57 +0100

    la balle bouge

--- pong.py
+++ pong.py
@@ -1,12 +1,43 @@
+from time import sleep
 from sense_hat import SenseHat
 
 sense = SenseHat()
 
-X = [255, 0, 0]  # Red
-O = [255, 255, 255]  # White
+sense.clear()
 
-question_mark = [
-x
-]
+red = (255, 0, 0)
 
-sense.set_pixels(question_mark)
+x = 3
+y = 5
+ysens = 1
+xsens = 1
+
+
+
+
+def dessine_ball():
+  global x, y, xsens, ysens
+  x = x + xsens
+  y = y + ysens
+
+  sense.set_pixel(x,y,red)
+
+  if x == 0:
+    xsens = 1
+  if x == 7:
+    xsens = -1
+
+  if  y == 0:
+    ysens = 1
+  elif y == 7:
+    ysens = -1
+
+
+while True:
+  sense.clear()
+  dessine_ball()
+
+
+  sleep(0.3)
+
+
